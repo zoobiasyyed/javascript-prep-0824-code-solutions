bc19435b5e383eaf57d9b44a91d0e6253556b6dd
Zoobia Syyed <syyedzoobia@gmail.com>
2024-08-08 01:29:50 +0000

    Completed prep javascript operators and expressions

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,55 @@
+// exercise 1
+
+const number1 = 20;
+const number2 = 30;
+const product = number1 * number2;
+console.log(product);
+console.log(typeof product);
+
+// exercise 2
+
+const charge = 40.67;
+const payment = 30.52;
+const amountRemaining = payment - charge;
+console.log(amountRemaining);
+console.log(typeof amountRemaining);
+
+// exercise 3
+
+const tests = 4;
+const assignment = 20;
+const final = 2;
+const grade = (tests + assignment + final) / 2;
+console.log(grade);
+console.log(typeof grade);
+
+// exercise 4
+
+const firstName = 'Zoobia';
+const lastName = 'Syyed';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+
+// exercise 5
+
+const pH = 3;
+let isAcidic = pH < 7;
+//if (pH < 7) {
+//  isAcidic = true;
+//}
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+// exercise 6
+
+numberOfSoldiers = 5000;
+let isSparta = numberOfSoldiers === 300;
+console.log(isSparta);
+console.log(typeof isSparta);
+
+//exercise 7
+
+let nickname = fullName + ' is the GOAT!';
+console.log(nickname);
+console.log(typeof nickname);
